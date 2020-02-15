88c2c128a5c7ca32a1dd7363847af44d9df0b9b4
Joan <joanandreusole@gmail.com>
2020-02-15 13:05:22 +0100

    Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import Translate from "../lang/Translate";
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h2>Home</h2>
+                <div className="row">
+                    <div className="videobackground">
+                        <div className="overlay"></div>
+                        <video playsinline="playsinline" autoPlay="autoplay" muted="muted" loop="loop">
+                            <source src="http://www.goatrails.dawman.info/img/video.mp4" type="video/mp4"/>
+                        </video>
+                        <div className="container h-100">
+                            <div className="d-flex h-100 text-center align-items-center">
+                                <div className="w-100 text-white">
+                                    <h1 className="display-4"><Translate string={'welcome'}/></h1>
+                                    <p className="lead mb-0"><Translate string={'keyline'}/></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
