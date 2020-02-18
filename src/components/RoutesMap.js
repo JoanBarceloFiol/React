6849ba71295ec1ce3bb40d994ecb5fedf6bfc636
Joan <joanandreusole@gmail.com>
2020-02-18 13:38:31 +0100

    refact RouteMap

--- src/components/RoutesMap.js
+++ src/components/RoutesMap.js
@@ -16,68 +16,21 @@ class RoutesMap extends Component {
         };
 
         this.getGeoJson = this.getGeoJson.bind(this);
-        this.createPoint = this.createPoint.bind(this);
-        this.testPoint = this.testPoint.bind(this);
-        this.asyncData = this.asyncData.bind(this);
     }
 
     componentDidMount(){
-        let data = this.asyncData();
+        let data = axios.get('http://localhost:80/api/first-points');
             data.then( res => {
                 const points = res.data;
                 this.setState({points, state: true});
             });
     }
 
-    async asyncData() {
-        // fetch data from a url endpoint
-        const data = axios.get('http://localhost:80/api/get/first-point-route/all');
-        return data;
-    }
-
     getGeoJson() {
-        let points = this.state.points.map(res => this.createPoint(res.x, res.y));
-        let rempGeoJson = this.testPoint(points);
-        let geoJsonPlant = {
-            "type": "FeatureCollection",
-            "features":[
-                0
-            ]
-
-        };
-
-        geoJsonPlant = JSON.stringify(geoJsonPlant);
-
         if (this.state.state)
-            return ( <GeoJSON data={JSON.parse(geoJsonPlant.replace('0', rempGeoJson))} />);
+            return ( <GeoJSON data={this.state.points} />);
     }
 
-
-    createPoint(x, y){
-        return {
-            "type": "Feature",
-            "properties": {},
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    parseFloat(x),
-                    parseFloat(y)
-                ]
-            }
-        }
-    }
-
-    testPoint(array){
-        let json = '';
-        let com = ',';
-        for ( let i = 0 ; i<array.length ; i++){
-            com = (i+1 === array.length) ? '' : ',';
-
-            json = json + JSON.stringify(array[i]) + com;
-        }
-
-        return json;
-    }
 
     render() {
         const position = [this.props.lat, this.props.lng];
